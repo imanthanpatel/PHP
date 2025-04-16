9724093369b8d913c338d60dc64dbe626ce8b5a0
iEcommerceweb <manthan11289@gmail.com>
2025-04-16 09:20:27 +0530

    php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $server ="localHost";
+    $username = "root";
+    $password = "";
+    $con = mysqli_connect($server,$username,$password)
+
+    if(!$con){
+        die("connection to this database failed due to" .mysqli_connect_error());
+    }
+
+    echo "Succes Connection to db";
+?>
